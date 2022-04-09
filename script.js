ba10391b6c5e5abdabf5243cd180429bbd923535
Corum Gold <goldcorum@gmail.com>
2022-04-08 20:49:26 -0400

    Create computerPlay function

--- script.js
+++ script.js
@@ -2,27 +2,33 @@
 let userScore = 0;
 let compScore = 0;
 
-let choices = ['ROCK', 'PAPER', 'SCISSORS'];
-let compChoice = choices[Math.floor(Math.random() * choices.length)];
-
-let input = prompt("Pick 'ROCK,' 'PAPER,' or 'SCISSORS'")
-
-let userChoice = input.toUpperCase();
-console.log(userChoice)
-
-if (userChoice == "ROCK" && compChoice == "PAPER" ||
-    userChoice == "SCISSORS" && compChoice == "ROCK" ||
-    userChoice == "PAPER" && compChoice == "SCISSORS") {
-    compScore++;
-    alert(`The computer won that round! It picked ${compChoice}.`)
+function computerPlay() {
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    return compChoice;
 }
 
-else if (userChoice == "ROCK" && compChoice == "SCISSORS" ||
-    userChoice == "SCISSORS" && compChoice == "PAPER" ||
-    userChoice == "PAPER" && compChoice == "ROCK") {
-    userScore++;
-    alert(`YOU won that round! The computer picked ${compChoice}.`)
+computerPlay()
 
-} else {
-    alert(`It's a tie! The computer picked ${compChoice}`)
-}
+
+// let input = prompt("Pick 'ROCK,' 'PAPER,' or 'SCISSORS'")
+
+// let userChoice = input.toUpperCase();
+// console.log(userChoice)
+
+// if (userChoice == "ROCK" && compChoice == "PAPER" ||
+//     userChoice == "SCISSORS" && compChoice == "ROCK" ||
+//     userChoice == "PAPER" && compChoice == "SCISSORS") {
+//     compScore++;
+//     alert(`The computer won that round! It picked ${compChoice}.`)
+// }
+
+// else if (userChoice == "ROCK" && compChoice == "SCISSORS" ||
+//     userChoice == "SCISSORS" && compChoice == "PAPER" ||
+//     userChoice == "PAPER" && compChoice == "ROCK") {
+//     userScore++;
+//     alert(`YOU won that round! The computer picked ${compChoice}.`)
+
+// } else {
+//     alert(`It's a tie! The computer picked ${compChoice}`)
+// }
